9e0ad55211324500f04ff7533e7c93e9b6d9df10
Blake Strickland <Stricklandb02@gmail.com>
2016-04-08 12:32:00 -0400

    read data and retrn user objects with user id age gener occupation and zip code

--- movie.py
+++ movie.py
@@ -19,6 +19,19 @@ class Rating:
     def __repr__(self):
         return str(self)
 
+class User:
+    def __init__(self, user_id, age, gender, occupation, zip_code):
+        self.user_id = user_id
+        self.age = age
+        self.gender = gender
+        self.occupation = occupation
+        self.zip_code = zip_code
+
+    def __str__(self):
+        return str(self.user_id + " " + self.age + " " + self.gender + " " + self.occupation + " " + self.zip_code )
+
+    def __repr__(self):
+        return str(self)
 
 
 
@@ -27,12 +40,19 @@ with open("u.item", encoding="latin_1") as item_file:
     reader = csv.DictReader(item_file, delimiter='|', fieldnames=['movie_id', 'title'])
     for row in reader:
         movie_obj_list.append(Movie(row['movie_id'], row['title']))
-
 # print(movie_obj_list)
+
+
 with open("u.data", encoding="latin_1") as item_file:
     rating_obj_list = []
     reader = csv.DictReader(item_file, delimiter='\t', fieldnames=['user_id', 'movie_id', 'rating'])
     for row in reader:
         rating_obj_list.append(Rating(row['user_id'], row['movie_id'], row['rating']))
+# print(rating_obj_list)
 
-print(rating_obj_list)
+with open("u.user", encoding="latin_1") as item_file:
+    user_obj_list = []
+    reader = csv.DictReader(item_file, delimiter='|', fieldnames=['user_id', 'age', 'gender', 'occupation', 'zip_code'])
+    for row in reader:
+        user_obj_list.append(User(row['user_id'], row['age'], row['gender'], row['occupation'], row['zip_code']))
+print(user_obj_list)
